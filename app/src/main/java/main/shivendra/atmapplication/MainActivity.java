bfcdc84162173f7fba5a5566164581ea48c46bcf
shivendra885 <shivendrarbl885@gmail.com>
2024-05-02 11:45:14 +0530

    Push Code

--- app/src/main/java/main/shivendra/atmapplication/MainActivity.java
+++ app/src/main/java/main/shivendra/atmapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,10 +39,46 @@ public class MainActivity extends AppCompatActivity {
         btnWithdraw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                System.out.println(textAmount + " " + amount);
-                int currAmo = Integer.parseInt(amount.getText().toString());
-                int withDrawAmo = Integer.parseInt(amount.getText().toString());
-                amount.setText(String.valueOf(currAmo-withDrawAmo));
+                String amountStr = textAmount.getText().toString();
+                if(amountStr.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter the withdrawal amount.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                int currAmount = Integer.parseInt(amount.getText().toString());
+                int withdrawAmount = Integer.parseInt(textAmount.getText().toString());
+
+                if (withdrawAmount > currAmount) {
+                    Toast.makeText(MainActivity.this, "Insufficient funds!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (withdrawAmount < 100) {
+                    Toast.makeText(MainActivity.this, "Minimum Withdraw Amount is : 100!", Toast.LENGTH_SHORT).show();
+                }
+                int remainingAmount = currAmount - withdrawAmount;
+                amount.setText(String.valueOf(remainingAmount));
+
+                int twentiesToDeduct = withdrawAmount / 2000;
+                withdrawAmount %= 2000;
+                int fiftiesToDeduct = withdrawAmount / 500;
+                withdrawAmount %= 500;
+                int fivesToDeduct = withdrawAmount / 200;
+                withdrawAmount %= 200;
+                int onesToDeduct = withdrawAmount / 100;
+
+                // Update the number of twenties, fifties, fives, and ones
+                int currentTwenties = Integer.parseInt(twenty.getText().toString());
+                int currentFifties = Integer.parseInt(five.getText().toString());
+                int currentFives = Integer.parseInt(two.getText().toString());
+                int currentOnes = Integer.parseInt(one.getText().toString());
+
+                // Subtract the calculated numbers of twenties, fifties, fives, and ones
+                twenty.setText(String.valueOf(currentTwenties - twentiesToDeduct));
+                five.setText(String.valueOf(currentFifties - fiftiesToDeduct));
+                two.setText(String.valueOf(currentFives - fivesToDeduct));
+                one.setText(String.valueOf(currentOnes - onesToDeduct));
+
+                textAmount.setText("");
             }
         });
     }

fc45189b90a25739ccbd5311454984b172c20335
shivendra885 <shivendrarbl885@gmail.com>
2024-05-02 08:23:22 +0530

    Code up to date 01-06-2024

--- app/src/main/java/main/shivendra/atmapplication/MainActivity.java
+++ app/src/main/java/main/shivendra/atmapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package main.shivendra.atmapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
+    TextView amount, one, two, five, twenty;
+    Button btnWithdraw;
+    EditText textAmount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        amount = findViewById(R.id.amount);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        five = findViewById(R.id.five);
+        twenty = findViewById(R.id.twenty);
+        btnWithdraw = findViewById(R.id.btnWithdraw);
+        textAmount = findViewById(R.id.textAmount);
+        btnWithdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                System.out.println(textAmount + " " + amount);
+                int currAmo = Integer.parseInt(amount.getText().toString());
+                int withDrawAmo = Integer.parseInt(amount.getText().toString());
+                amount.setText(String.valueOf(currAmo-withDrawAmo));
+            }
         });
     }
 }
